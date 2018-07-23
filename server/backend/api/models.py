6321b2c95e2bbb5f693a5106f08cf2a686f23771
a-wro <aleksy.wroblewski@onet.pl>
2018-07-23 22:31:53 +0200

    checklist

--- server/backend/api/models.py
+++ server/backend/api/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Checklist(models.Model):
+    name = models.CharField(max_length=50)
+    added = models.DateField(auto_now_add=True)
+
 class Todo(models.Model):
     task = models.CharField(max_length=50)
     added = models.DateField(auto_now_add=True)
     completed = models.BooleanField(default=False)
-
-class Checklist(models.Model):
-    todos = models.ForeignKey(Todo, on_delete=models.CASCADE)
-    added = models.DateField(auto_now_add=True)
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
 
 
